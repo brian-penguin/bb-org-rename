c2dde6ba15c7fe2aec4dedbc971b55152c3c7454
brimatteng <btenggren20@gmail.com>
2021-08-20 10:32:48 -0400

    Store all the data we have about a repo!

--- scripts/download_repos.clj
+++ scripts/download_repos.clj
@@ -24,25 +24,16 @@
 
 ; For debugging
 (defn parse-response [response]
-  (let [body (json/parse-string (:body response))
+  (let [body (json/parse-string (:body response) true)
          status (:status response)
          headers (:headers response)]
-    ;(prn "RESPONSE ------------")
-    ;(pprint status)
+    (prn "RESPONSE ------------")
+    (pprint status)
     ;(pprint headers)
     ;(pprint (first body))
-    ;(prn "--------------------")
+    (prn "--------------------")
     body))
 
-(defn parse-repo-data[repo-data]
-  {:id (get repo-data "id")
-   :name (get repo-data "name")
-   :url (get repo-data "url")
-   :default-branch (get repo-data "default_branch")
-   :description (get repo-data "description")})
-
-(defn body->values [resp-body]
-  (map parse-repo-data resp-body))
 
 (defn request-repo-page [page]
   (-> (curl/request {:basic-auth [username user-auth-token]
@@ -53,12 +44,26 @@
                            :path   (str "/orgs/" org-name "/repos")
                            :method "get"
                            :query  (str "per_page=100&page=" page)}})
-    parse-response
-    body->values))
+      parse-response))
 
 (def page-range (map inc (range 12)))
 (defn all-repos [] (map request-repo-page page-range))
 
 
 ;; Store the repo data so I don't have to run this again
-(spit "output/repos.edn" (pr-str (flatten (all-repos))))
+(spit "output/repos-full.edn" (pr-str (flatten (all-repos))))
+
+; Just the one repo actually
+;(defn request-repo-page [repo-name]
+  ;(-> (curl/request {:basic-auth [username user-auth-token]
+                     ;:headers {"Accept" "application/vnd.github.v3+json"}
+                     ;:url {:scheme "https"
+                           ;:host   "api.github.com"
+                           ;:port   443
+                           ;:path   (str "/repos/" username "/" repo-name)
+                           ;:method "get"}})
+      ;parse-response))
+
+
+;(spit "output/repos-single.edn" (pr-str (request-repo-page "ring-app-example" )))
+
